6533b58f8ff91cfd54aa71a7f20808b95cc5faa5
shin☘ <huutrimega@gmail.com>
2021-01-28 21:39:02 +0700

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('todo api')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const documents = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documents);
+  await app.listen(port);
 }
 bootstrap();
