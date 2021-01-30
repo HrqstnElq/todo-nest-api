598c967ee172a9ef0b0242c888c36ec2f3feb4e7
shin☘ <huutrimega@gmail.com>
2021-01-30 12:55:50 +0700

    add view engine

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,31 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   //allow all origin, header, ...
   app.enableCors({});
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  //set static folder
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  //set view engine
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('todo api')
     .setVersion('1.0')
-    .setBasePath('/')
+    .setBasePath('api')
     .addBearerAuth()
     .build();
 
   const documents = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('', app, documents);
+  SwaggerModule.setup('api', app, documents);
   await app.listen(port);
 }
 bootstrap();
